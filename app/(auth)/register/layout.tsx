637894b53463cdffed4a8ff01d5bc511dcb8abe1
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2023-07-27 15:30:33 -0300

    update flow ci/cd

--- app/(auth)/register/layout.tsx
+++ app/(auth)/register/layout.tsx
@@ -19,7 +19,7 @@ export default function DashboardRootLayout({children}: PropsWithChildren) {
         <div className="flex min-h-full flex-col justify-center pb-32 pt-20">
           <div className="mx-auto w-full max-w-md">
             <div className="flex flex-col gap-3 text-center">
-              <h1 className="text-h1">Let's start your journey!</h1>
+              <h1 className="text-h1">Let&apos;s start your journey!</h1>
               <p className="text-body-md text-night-200">
                 Please enter your details.
               </p>
