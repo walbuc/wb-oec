57aa96c3afd9811eaf3481a4b46c01e99214fa42
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2023-07-28 10:30:04 -0300

    add error ignore beause of test bot

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -3,7 +3,6 @@ import {userBuilder} from '../support/generate'
 describe('registration', () => {
   it('should register a new user', () => {
     const user = userBuilder()
-    console.log(user, 'Userrrr')
     cy.visit('/register')
     cy.findByLabelText(/email/i).type(user.email)
     cy.findByLabelText(/password/i).type(user.password)
