f090ef75755ab84fac6af1edd3a187a0c5236d00
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2023-07-28 10:26:52 -0300

    update extension

--- cypress/support/generate.ts
+++
@@ -1,8 +0,0 @@
-import {build, fake} from 'test-data-bot'
-
-const userBuilder = build('User').fields({
-  email: fake(f => f.internet.email()),
-  password: fake(f => f.internet.password()),
-})
-
-export {userBuilder}
