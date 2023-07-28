62ac755aa5131f0fad052e1a0ac043867c5b9f85
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2023-07-28 19:38:57 -0300

    renmae

---
+++ app/(dashboard)/home/countries.tsx
@@ -0,0 +1,15 @@
+import {getCountries} from '@/lib/client'
+import CountrySearchCombobox from '@/components/country-combobox'
+import {delay} from '@/lib/async'
+
+export default async function ComponentTEst() {
+  //await delay(4000)
+  const items = await getCountries()
+
+  return (
+    <CountrySearchCombobox
+      placeholder="Search for a country"
+      itemsInitial={items}
+    />
+  )
+}
