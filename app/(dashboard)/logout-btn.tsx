11d139ef0432852a792a83018d9eb6ec4b0212c4
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2023-07-31 20:51:23 -0300

    update config

--- app/(dashboard)/logout-btn.tsx
+++ app/(dashboard)/logout-btn.tsx
@@ -1,19 +1,21 @@
 'use client'
-//import {logout} from '../actions'
+import {logout} from '../actions'
 import {Button} from '@/components/forms'
 import {useRouter} from 'next/navigation'
 import {useTransition} from 'react'
-import {logout} from '@/lib/client'
+//import {logout} from '@/lib/client'
 function Logout() {
   let [isPending, startTransition] = useTransition()
   const router = useRouter()
 
   return (
     <Button
-      onClick={async () => {
-        await logout()
-        router.push('/signin')
-      }}
+      onClick={() =>
+        startTransition(async () => {
+          await logout()
+          router.push('/signin')
+        })
+      }
       size="sm"
       variant="primary"
     >
