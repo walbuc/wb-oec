8ba99240e013c365592ee08b0c937cd6eb8879e0
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2023-07-31 19:08:30 -0300

    updte main

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
 import {NextResponse} from 'next/server'
 import type {NextRequest} from 'next/server'
-// works at edge runtime web workers
 import {jwtVerify} from 'jose'
 const PUBLIC_FILE = /\.(.*)$/
 
-// had to make this again here as the other one is in a file with bcrypt which is not supported on edge runtimes
 const verifyJWT = async (jwt: string) => {
   const {payload} = await jwtVerify(
     jwt,
     new TextEncoder().encode('somesecretevaluejwt'),
   )
+
   return payload
 }
 
 export default async function middleware(req: NextRequest) {
   const {pathname} = req.nextUrl
+
   if (
     pathname.startsWith('/_next') ||
     pathname.startsWith('/api') ||
@@ -25,9 +25,10 @@ export default async function middleware(req: NextRequest) {
   ) {
     return NextResponse.next()
   }
-  //we validate here if we have alrearya token in the cookie session
+
   const jwt = req.cookies.get('__cookie_custom_name')
-  // go away
+  console.log(jwt)
+
   if (!jwt) {
     req.nextUrl.pathname = '/signin'
     return NextResponse.redirect(req.nextUrl)
@@ -35,16 +36,9 @@ export default async function middleware(req: NextRequest) {
 
   try {
     await verifyJWT(jwt.value)
-    // here you go
-    if (pathname === '/') {
-      req.nextUrl.pathname = '/home'
-      return NextResponse.redirect(req.nextUrl)
-    }
     return NextResponse.next()
   } catch (e) {
-    console.error(e)
     req.nextUrl.pathname = '/signin'
-    // go away
     return NextResponse.redirect(req.nextUrl)
   }
 }
